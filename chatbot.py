58f52e932309b53fe085b8ca88d3bcb571fe2a5e
Cedric Tong <ctong@calstatela.edu>
2017-10-21 05:18:39 -0700

    removed trash

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,3 @@
-'''
-Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-    http://aws.amazon.com/apache2.0/
-
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-'''
-
 import cfg
 import sys
 import irc.bot
@@ -250,7 +240,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 
         elif cmd == 'help':
              c.privmsg(self.channel, 'poll, disspoll, endpoll, vote, peopleschoices, fav_artist, playing. tumblr, tweet, joke, game, title, schedule')
-             
+
         else:
             c.privmsg(self.channel, "Did not understand command: " + cmd)
 
